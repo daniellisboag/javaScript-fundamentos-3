a99c5b7016eeab852a383baa4b5242e40a81d962
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-06-27 19:03:04 -0300

    Criando objetos

---
+++ objeto.js
@@ -0,0 +1,11 @@
+const listaCPFs = ['11348273819', '14857382930'];
+
+const infosPessoa = ['nome', 'José Silva', 'idade', 32];
+console.log(infosPessoa[1]);
+
+const objPessoa = {
+   nome: 'José Silva',
+   idade: 32,
+   cpf: '38472637281',
+   turma: 'JavaScript'
+};
