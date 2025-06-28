e75bab06300e52d53859dfed249927769c6c2869
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-06-28 19:31:11 -0300

    Adicionando e alterando objetos

---
+++ alterando-objeto.js
@@ -0,0 +1,18 @@
+const estudante = {
+   nome: 'José Silva',
+   idade: 32,
+   cpf: '38472637281',
+   turma: 'JavaScript'
+};
+
+estudante.telefone = '551199999999';
+console.log(estudante.telefone);
+console.log(estudante);
+
+estudante.nome = 'José Souza';
+console.log(estudante);
+
+const estudante2 = {};
+estudante2.nome = 'Ana Maria';
+
+console.log(estudante2.nome);
