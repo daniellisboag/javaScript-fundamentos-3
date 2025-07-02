c5a7f931bb2e66d72c7d22abfa9f4b76d8c7c5e7
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-07-01 21:47:03 -0300

    Tipos de dados e valores

---
+++ dados-valores.js
@@ -0,0 +1,10 @@
+const estudante = {
+   nome: 'José Silva',
+   idade: 32,
+   cpf: '12312312300',
+   turma: 'JavaScript',
+   bolsista: true,
+   telefone: ['5511999999998', '5511999999993']
+}
+
+console.log(estudante.telefone);
