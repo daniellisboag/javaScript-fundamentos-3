fb3392d718e65f49b72dc52e7f3afd94b13713c0
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-07-17 19:31:56 -0300

    Convertendo objeto em string

---
+++ aula4-exercicio3.js
@@ -0,0 +1,24 @@
+/* 3. Crie um objeto JavaScript representando informações de um produto. O objeto deve conter pelo menos as seguintes propriedades:
+
+   id (number): identificador do produto.
+   nome (string): nome do produto.
+   preco (number): preço do produto.
+
+Exemplo
+const produto = {
+    id: 1,
+    nome: "Camiseta",
+    preco: 25.99
+}
+
+Converta o objeto produto para uma string utilizando JSON.stringify() e imprima no console a string resultante. */
+
+const produto = {
+   id: 1,
+   nome: 'Camiseta',
+   preco: 25.99
+};
+
+const converter = JSON.stringify(produto);
+
+console.log(converter);
