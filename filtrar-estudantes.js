7b60efa1462079cab2067f3934ddaa1c58a5e3d4
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-07-19 17:49:19 -0300

    Encontrando um objeto

---
+++ filtrar-estudantes.js
@@ -0,0 +1,11 @@
+const estudantes = require('./estudantes.json');
+
+function filtrarPorPropriedade(lista, propriedade) {
+   return lista.filter((estudante) => {
+      return !estudante.endereco.hasOwnProperty(propriedade);
+   })
+}
+
+const listaEnderecosIncompletos = filtrarPorPropriedade(estudantes, 'cep');
+
+console.log(listaEnderecosIncompletos);
